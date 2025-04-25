babfee778fa6db03e7878451b4993f036041ed04
veramaximiliano <vera.maximiliano@gmail.com>
2025-04-25 18:04:46 -0300

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <HashRouter>
+    <Router basename="/pasquala">
         <App />
-    </HashRouter>
+    </Router>
 )
